eaead952f22c5d3e80c2ec8b84cf09e61c158740
HeartBtz <67628834+HeartBtz@users.noreply.github.com>
2022-05-21 00:19:32 +0200

    Excluded devices Fixed
    no discord message if there's only excluded messages.

--- Install.bat
+++ Install.bat
@@ -1,4 +1,2 @@
-@echo off
-mkdir C:\Users\Public\Documents\EarnappOfflineMonitor\
 setx EarnAppLogPath C:\Users\Public\Documents\EarnappOfflineMonitor\
 setx EarnAppPath  %~dp0%
--- LectureJSON.ps1
+++ LectureJSON.ps1
@@ -17,13 +17,13 @@
 #                                                                                                                                        #
 #                     =====================Informations WebHook Discord==========================                                        #
 #                                                                                                                                        #
-$oAuthRefreshToken = 'ur Token Here'                                                                                                     #
+$oAuthRefreshToken = 'auth token'       #
 $AvatarImageURL = 'https://cdn.discordapp.com/attachments/972197177029980172/975730277139746826/progyblue.png'                           #
 $AvatarName = "Monitor-Senseii"                                                                                                          #
-$WebHookUrl = 'ur WebHook Link Here'                                                                                                     #
-$ExcludedDevices = [System.Collections.ArrayList]@("DeviceName1","DeviceName2","DeviceName..","DeviceNameX")                             # 
-#                                                  Replace Devices Name by ur Devices.                                                   #
+$WebHookUrl = 'webhook link' #
+$ExcludedDevices = [System.Collections.ArrayList]@("DeviceName1","DeviceName2","DeviceName...","DeviceNameX","2Win5")                   #                                                                                                                                        #
 #                                                                                                                                        #
+#                                                                                                                                        #                                                                                                                                        #
 ##########################################################################################################################################
 
 function ObtainDate{
@@ -44,7 +44,7 @@ $scriptWay = $env:EarnAppPath # Cherche le chemin dans lequel les scripts sont.
 $resultsLogDir = $env:EarnAppLogPath
 $OutputVar = node ( $scriptWay + "Infos.js" ) $oAuthRefreshToken | Out-String #Lance le script JavaScript & envoie le résultat dans la variable $OutputVar => chemin du fichier JSON
 if (Test-Path ($resultsLogDir + "results.json")){
-    MakeLogMessage('JSON récupéré avec succès')
+    MakeLogMessage('JSON récupéré avec succès')#Logs
     $inputJson = Get-Content ($resultsLogDir + 'results.json' ) | ConvertFrom-Json #Lecture & Inteprétation de results.json ($OutputVar) || Reading & Interprate of results.json
     $deviceNumber = $inputJson[0].PSObject.Properties.Name.Length #Nombre de Machines || Number of Devices 
     $devicesList = [System.Collections.ArrayList]@() #Tableau vide
@@ -58,29 +58,25 @@ if (Test-Path ($resultsLogDir + "results.json")){
         }  
     }
     $offlineDeviceNumber = $offlineDevices.Count #Nombre de Machines offline || Number of offline devices
-    MakeLogMessage("Il y a $offlineDeviceNumber client(s) déconnecté(s) ")
+    MakeLogMessage("Il y a $offlineDeviceNumber client(s) déconnecté(s) ")#Logs
     #$isExcluded = [System.Collections.ArrayList]@() #Tableau vide
     if($offlineDeviceNumber -ne 0){ # Si le nombre de machines éteintes est différent de zéro
         MakeLogMessage("Un message va donc être envoyé sur Discord")
-       
+        $Exclusion = '0'
         For ($j = 0; $j -lt $offlineDeviceNumber; $j++){
             $device = [string]$offlineDevices[$j]
             if ($device -in $ExcludedDevices){ #si y'a le device qui apparaît est dans la liste d'exclusion alors...
-                $device
-                $ExcludedDevices
-                $Name = ":green_square: **Device:** " + $inputJson[0].$device.title + " :green_square:" #ça osef
-                $Value = ":recycle: **Excluded** " # ça aussi
-                $temp3 =  $Fact.Add($j) #ça encore
+                $Exclusion++
             }else{ #sinon...
-                $device
-                $ExcludedDevices
-                $Name = ":red_square: **Device:** " + $inputJson[0].$device.title + " :red_square:" #on s'en fout encore....
+                $Name = ":red_square: **Device:** " + $inputJson[0].$device.title + " :red_square:" 
                 $Value = ":biohazard:**UUID:**   " + $inputJson[0].$device.uuid + " 
-                :globe_with_meridians:**IP:**   " + $inputJson[0].$device.ips #et toujours....
-                $temp3 =  $Fact.Add($j) #fin du je m'en foutisme
+                :globe_with_meridians:**IP:**   " + $inputJson[0].$device.ips #laisser la ligne sautée, c'est pour la présentation de l'embed dans discord
+                $temp3 =  $Fact.Add($j) 
+                $Fact[$j] = New-DiscordFact -Name $Name -Value $Value -Inline $false
             }
-            $Fact[$j] = New-DiscordFact -Name $Name -Value $Value -Inline $false #peu importe
         }
+        MakeLogMessage("$Exclusion Appareils ont été exclus")
+
         #DISCORD BOT SECTION | Consultez la Doc des devs pour plus d'informations sur la Librairie: https://evotec.xyz/hub/scripts/psdiscord-powershell-module/
         $Author = New-DiscordAuthor -Name $AvatarName -IconUrl $AvatarImageURL
         $Section = New-DiscordSection -Title "$(Get-Date -Format 'HH:mm') | $offlineDeviceNumber Device(s) offline: " -Facts $Fact -Color BlueViolet -Author $Author -Thumbnail $Thumbnail 
